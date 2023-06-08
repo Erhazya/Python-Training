a5a0b26d450517ceca697ef9c4a83fe6f8eaa68f
Andy Longrée <longree.andy13@gmail.com>
2023-06-09 00:48:02 +0200

    upload

--- Udemy/Exercices/erreur_ouverture_fichier.py
+++ Udemy/Exercices/erreur_ouverture_fichier.py
@@ -1,37 +1,29 @@
 from pathlib import Path
 
+fichier01 = Path("././FichierTest/readme.txt")
 
+fichier02 = Path("././FichierTest/readm.txt")
 
-fichier01 = Path("./FichierTest/readme.txt")
+fichier03 = Path("C:/Users\Se7en\Documents\GitHub\Python-Training\FichierTest/fichier_invalide.abc")
 
-fichier02 = Path("./FichierTest/readm.txt")
-
-fichier03 = Path("./FichierTest/fichier_invalide.abc")
-
-
-
-
-try :
-    with open(fichier01,"r") as f :
+try:
+    with open(fichier01, "r") as f:
         contenu = f.read()
         print(contenu)
 except:
     print("Erreur dans le chargement du fichier")
 
-
-
-try :
-    with open(fichier02,"r") as f :
+try:
+    with open(fichier02, "r") as f:
         contenu = f.read()
         print(contenu)
-except FileNotFoundError as e :
+except FileNotFoundError as e:
     print(e.filename)
     print("Fichier introuvable")
 
-
-try :
-    with open(fichier03,"r") as f :
+try:
+    with open(fichier03, "r") as f:
         contenu = f.read()
         print(contenu)
-except UnicodeDecodeError :
+except UnicodeDecodeError:
     print("Impossible d'ouvvrir le fichier")
